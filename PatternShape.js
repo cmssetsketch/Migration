80d7b189cf2dfa26f29840f8454256d82f404c8e
cmssetsketch <cmsset@gmail.com>
2026-02-18 17:36:56 +0100

    Update shape group '02' with new country
    Added 'Madagascar' to shape group '02' and removed duplicate entry.

--- PatternShape.js
+++ PatternShape.js
@@ -9,7 +9,7 @@ const shapeNames = [
 
 const shapeGroups = {
   "01": ["American Samoa" , "United States of America", "Botswana", "Guam" ,"New Zealand", "Laos","Hong Kong", "Nepal", "Tadjikistan","Yemen", "Peru", "Puerto Rico", "Mexico", "Guinea", "Democratic Republic of the Congo", "South Sudan", "Italy", "Czechia", "Netherlands" , "Denmark"],
-  "02": ["Wallis and Futuna Islands" , "Kiribati", "New Caledonia" ,"Myanmar", "Japan" , "India" ,"Iraq" ,"Ecuador" , "Dominica" , "Canada" , "Niger"  , "Chad"  , "South Africa", "Libya" ,"Croatia" ,  "Belgium",  "Ukraine"],
+  "02": ["Wallis and Futuna Islands" , "Kiribati", "New Caledonia" ,"Myanmar", "Japan" , "India" ,"Iraq" ,"Ecuador" , "Dominica" , "Canada" , "Niger"  , "Chad"  , "South Africa","Madagascar", "Libya" ,"Croatia" ,  "Belgium",  "Ukraine"],
  "03": [ "Bahrain"  ,"United States Virgin Islands", "Mayotte" ,"Montenegro"],
    "04": ["Spain"  , "Cook Islands" ,"Bangladesh" ,"Saudi Arabia" ,"Brazil" ,"Martinique" ,"Honduras" , "Nigeria" ,"Cameroon", "Djibouti" ,"Morocco" , "Slovakia" ,  "Ireland"],
  "05": ["Georgia" , "Réunion" , "Saint Kitts and Nevis","Isle of Man"],
@@ -20,7 +20,7 @@ const shapeGroups = {
  "10": ["Qatar" ,"Mauritius" ,"Antigua and Barbuda","Channel Islands" ],  
   "11": ["Pakistan" ,"Armenia" ,  "Uruguay"  , "Trinidad and Tobago", "Saint Helena", "Zimbabwe"  ,"Romania", "Serbia"],
   "12": ["Northern Mariana Islands" , "Fiji" ,"Viet Nam" ,"Sri Lanka" ,  "Kyrgyzstan" , "China"  , "Türkiye" ,"Bolivia" , "Cuba" ,  "Guatemala" ,"Saint Pierre and Miquelon" , "Côte d'Ivoire" , "Central African Republic"  , "Namibia" , "Eritrea" , "Sudan" ,  "Greece" ,  "Poland" ,"Switzerland","Sweden"],
- "13": [ "French Polynesia" , "Australia" ,"Cambodia" , "Macao" , "Bhutan" ,"Syria" ,"French Guiana" ,   "Guadeloupe", "Belize", "Bermuda" , "Madagascar" ,  "Burkina Faso" , "Angola", "Ethiopia" ,"Tunisia"  , "Hungary" ,"France", "United Kingdom","Portugal"], 
+ "13": [ "French Polynesia" , "Australia" ,"Cambodia" , "Macao" , "Bhutan" ,"Syria" ,"French Guiana" ,   "Guadeloupe", "Belize", "Bermuda"  ,  "Burkina Faso" , "Angola", "Ethiopia" ,"Tunisia"  , "Hungary" ,"France", "United Kingdom","Portugal"], 
   "14": ["Palau" ,  "Tonga" ,"Philippines" ,"Taiwan", "Uzbekistan"  , "Oman" ,"Cayman Islands", "Sierra Leone" , "Burundi", "Kosovo"],
      "15": ["Kuwait"  , "Paraguay" ,"Barbados" , "Ghana" ,"Mozambique","Iran"],
   "16": ["Anguilla" ],
